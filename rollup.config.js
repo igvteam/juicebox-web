5c1bf10074682da5dbe04ff2015abb83dfa5176a
Jim Robinson <jrobinso@broadinstitute.org>
2020-03-03 13:50:48 -0800

    build for "dist" folder

--- rollup.config.js
+++ rollup.config.js
@@ -1,65 +1,21 @@
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
-import strip from 'rollup-plugin-strip';
 import copy from 'rollup-plugin-copy'
-import {terser} from "rollup-plugin-terser"
 
 export default [
     {
         //input: 'test/testBabel.js',
         input: 'js/app.js',
         output: [
-            {file: 'dist/js/jb-bundle.esm.js', format: 'es'},
-            {file: 'dist/js/jb-bundle.esm.min.js', format: 'es', sourcemap: true},
+            {file: 'dist/js/app-bundle.esm.js', format: 'es'},
         ],
         plugins: [
-            strip({
-                debugger: true,
-                functions: ['console.log', 'assert.*', 'debug']
-            }),
-            terser({
-                include: [/^.+\.min\.js$/],
-                sourcemap: {
-                    filename: "juicebox.esm.min.js",
-                    url: "juicebox.esm.min.js.map"
-                }}),
             copy({
                 targets:
                     [
                         {src: 'node_modules/juicebox.js/dist/css/juicebox.css', dest: 'dist/css/'},
-                        {src: 'node_modules/juicebox.js/dist/css/img', dest: 'dist/css/'}
+                        {src: 'node_modules/juicebox.js/dist/css/img', dest: 'dist/css/'},
+                        {src: 'css/site.css', dest: 'dist/css/'}
                     ]
             })
         ]
     }
-    // ,
-    // {
-    //     input: 'js/site.js',
-    //     output: [
-    //         {file: 'dist/js/site-bundle.js', format: 'umd', name: "hicSite"},
-    //     ],
-    //     plugins: [
-    //         strip({
-    //             debugger: true,
-    //             functions: ['console.log', 'assert.*', 'debug']
-    //         }),
-    //         commonjs(),
-    //         resolve(),
-    //         babel(),
-    //         // terser({
-    //         //     include: [/^.+\.min\.js$/],
-    //         //     sourcemap: {
-    //         //         filename: "juicebox.min.js",
-    //         //         url: "juicebox.min.js.map"
-    //         //     }}),
-    //         copy({
-    //             targets:
-    //                 [
-    //                     {src: 'node_modules/juicebox.js/dist/css/juicebox.css', dest: 'dist/css/'},
-    //                     {src: 'node_modules/juicebox.js/dist/css/img', dest: 'dist/css/'}
-    //                 ]
-    //         })
-    //     ]
-    // }
 ];
---
+++ scripts/copyArtifacts.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const fs = require('fs-extra');
+
+const indexPath =  __dirname + '/../juicebox.html';
+let ping = fs.readFileSync(indexPath, 'utf-8');
+const lines = ping.split(/\r?\n/);
+
+const out = __dirname + '/../dist/juicebox.html';
+var fd = fs.openSync(out, 'w');
+
+for (let line of lines) {
+
+    if(line.includes("<script") && line.includes("module") && line.includes("app.js")) {
+        fs.writeSync(fd, '<script src="js/app-bundle.esm.js" type="module"></script>\n', null, 'utf-8');
+    } else   if(line.includes("juicebox.css")) {
+        fs.writeSync(fd, '<link rel="stylesheet" href="css/juicebox.css"/>\n', null, 'utf-8');
+    } else {
+        fs.writeSync(fd, line + '\n', null, 'utf-8')
+    }
+}
