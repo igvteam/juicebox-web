e0dc1ae3a8592d23a8d1aac8324a7f7e818e9a55
Jim Robinson <jrobinso@broadinstitute.org>
2020-04-02 23:28:15 -0700

    update config template

--- juiceboxConfig.js
+++ juiceboxConfig.js
@@ -1,13 +1,14 @@
 /*
  * @author Jim Robinson Nov-2019
  */
+/*
+ * @author Jim Robinson Nov-2019
+ */
 
-const juiceboxConfig = {
+var juiceboxConfig = {
     mapMenu: {
         id: 'contact-map-datalist',
-        // items: 'https://aidenlab.org/juicebox/res/mapMenuData.txt'
         items: 'https://aidenlab.org/juicebox/res/hicfiles.json'
-        // items: 'hicfiles.json'
     },
     trackMenu: {
         id: 'annotation-datalist',
@@ -18,25 +19,30 @@ const juiceboxConfig = {
         items: 'https://hicfiles.s3.amazonaws.com/internal/tracksMenu_2D.$GENOME_ID.txt'
     },
 
-    urlShortener:
-        [
-            {
-                provider: 'tinyURL'
-            }
-        ],
+    // List of URL shorteners.  First in the list is the default and will be used for shortening URLs
+    // Others potentiall used for expanding short URLs.  At least 1 shortener is required for the
+    // "Share" button.
+    urlShortener: [
+        {
+            provider: 'tinyURL'
+        },
+        // {
+        //     provider: "bitly",
+        //     apiKey: "BITLY ACCESS TOKEN HERE",
+        //     hostname: 'bit.ly'
+        // }
+    ],
 
     // Supply a Google client id to enable loading of private Google files.  Supply an API key to
     // enable loading of public Google files without login.
-    google: {
-        clientId: "GOOGLE_CLIENT_ID",  // TODO -- replace with your Google client ID (for oAuth)
-        apiKey: "GOOGLE_KEY",   // TODO -- replace with your Google API Key
-        scope:
-            [
-                'https://www.googleapis.com/auth/devstorage.read_only',
-                'https://www.googleapis.com/auth/userinfo.profile',
-                'https://www.googleapis.com/auth/drive.readonly'
-            ]
-    }
-};
-
-// export { juiceboxConfig }
+    // google: {
+    //     clientId: "GOOGLE CLIENT ID",
+    //     apiKey: "GOOGLE API KEY",
+    //     scope:
+    //         [
+    //             'https://www.googleapis.com/auth/devstorage.read_only',
+    //             'https://www.googleapis.com/auth/userinfo.profile',
+    //             'https://www.googleapis.com/auth/drive.readonly'
+    //         ]
+    // }
+}
