48adc9e0cf50127d302f6a92509b7b737f4cacb1
Jim Robinson <jrobinso@broadinstitute.org>
2020-04-03 14:47:36 -0700

    Update version Automate version string in html

--- scripts/copyArtifacts.js
+++ scripts/copyArtifacts.js
@@ -1,8 +1,11 @@
 #!/usr/bin/env node
 
 const fs = require('fs-extra');
-
 const indexPath =  __dirname + '/../juicebox.html';
+const pj = require.resolve('../package.json');
+const jsonText = fs.readFileSync(pj, 'utf-8');
+const version = JSON.parse(jsonText).version;
+
 let ping = fs.readFileSync(indexPath, 'utf-8');
 const lines = ping.split(/\r?\n/);
 
@@ -15,7 +18,11 @@ for (let line of lines) {
         fs.writeSync(fd, '<script src="js/app-bundle.esm.js" type="module"></script>\n', null, 'utf-8');
     } else   if(line.includes("juicebox.css")) {
         fs.writeSync(fd, '<link rel="stylesheet" href="css/juicebox.css"/>\n', null, 'utf-8');
-    } else {
+    } else if (line.includes("@VERSION")) {
+        line = line.replace("@VERSION", version);
+        fs.writeSync(fd, line + '\n', null, 'utf-8');
+    }
+    else {
         fs.writeSync(fd, line + '\n', null, 'utf-8')
     }
 }
