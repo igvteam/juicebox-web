ad816dbc6bf7f8d0a7afba596fc8ddb803c56aa8
Jim Robinson <jrobinso@broadinstitute.org>
2020-05-17 21:18:07 -0700

    .

--- juiceboxConfig.js
+++ juiceboxConfig.js
@@ -1,3 +1,10 @@
+/*
+ * @author Jim Robinson Nov-2019
+ */
+/*
+ * @author Jim Robinson Nov-2019
+ */
+
 var juiceboxConfig = {
     mapMenu: {
         id: 'contact-map-datalist',
@@ -15,28 +22,27 @@ var juiceboxConfig = {
     // List of URL shorteners.  First in the list is the default and will be used for shortening URLs
     // Others potentiall used for expanding short URLs.  At least 1 shortener is required for the
     // "Share" button.
-    // NOTE: you must provide an API key (Google) or access token (Bitly) to use these services on your site
     urlShortener: [
         {
             provider: 'tinyURL'
         },
-        {
-            provider: "bitly",
-            apiKey: "9915aab09b02b0c1a277c9af47e4bbcf0ab3f60f",
-            hostname: 'bit.ly'
-        }
+        // {
+        //     provider: "bitly",
+        //     apiKey: "BITLY ACCESS TOKEN HERE",
+        //     hostname: 'bit.ly'
+        // }
     ],
 
     // Supply a Google client id to enable loading of private Google files.  Supply an API key to
     // enable loading of public Google files without login.
-    google: {
-        clientId: "661332306814-fmasnut050v7qds33tsa2rtvd5tc06sl.apps.googleusercontent.com",  // TODO -- replace with your Google client ID (for oAuth)
-        apiKey: "AIzaSyCEmqU2lrAgKxJCbnJX87a5F3c9GejCCLA",   // TODO -- replace with your Google API Key
-        scope:
-            [
-                'https://www.googleapis.com/auth/devstorage.read_only',
-                'https://www.googleapis.com/auth/userinfo.profile',
-                'https://www.googleapis.com/auth/drive.readonly'
-            ]
-    }
+    // google: {
+    //     clientId: "GOOGLE CLIENT ID",
+    //     apiKey: "GOOGLE API KEY",
+    //     scope:
+    //         [
+    //             'https://www.googleapis.com/auth/devstorage.read_only',
+    //             'https://www.googleapis.com/auth/userinfo.profile',
+    //             'https://www.googleapis.com/auth/drive.readonly'
+    //         ]
+    // }
 }
