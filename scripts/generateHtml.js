04b6d366ad3020aef28eb6c4ed25fa71937a1545
Douglass Turner <douglass.turner@gmail.com>
2023-03-17 17:26:06 -0400

    Aiden Lab build script

--- scripts/generateHtml.js
+++ scripts/generateHtml.js
@@ -5,14 +5,16 @@ const pj = require.resolve('../package.json');
 const jsonText = fs.readFileSync(pj, 'utf-8');
 const version = JSON.parse(jsonText).version;
 
-
 const lines = fs.readFileSync(templatePath, 'utf-8').split(/\r?\n/);
 
-const outputFileName = process.argv.length > 3 ? process.argv[3] : 'juicebox.html'
+const outputFileName = process.argv[ 3 ]
+
+// node scripts/generateHtml.js ./navbars/aidenlab.html juicebox.html
+console.log(`argv ${ process.argv.length } aiden lab additions ${ process.argv[ 2 ] } output file ${ outputFileName }`)
 
 const out = __dirname + '/../dist/' + outputFileName;
 const fd = fs.openSync(out, 'w');
-let skipNavbar = false;
+let skipAidenLabAdditions = false;
 for (let line of lines) {
 
     if(line.includes("<script") && line.includes("module") && line.includes("app.js")) {
@@ -29,18 +31,18 @@ for (let line of lines) {
         fs.writeSync(fd, line + '\n', null, 'utf-8');
     }
 
-    // else if (line.includes("<!--NAVBAR-->") && process.argv.length > 2) {
-    //     const navbarFile = require.resolve(process.argv[2]);
-    //     const navbar = fs.readFileSync(navbarFile, 'utf-8');
-    //     fs.writeSync(fd, navbar, null, 'utf-8');
-    //     skipNavbar = true;
-    // }
-    //
-    // else if(skipNavbar) {
-    //     if(line.includes("<!--NAVBAR")) {
-    //         skipNavbar = false;
-    //     }
-    // }
+    else if (line.includes("<!--AIDEN_LAB-->")) {
+        const file = require.resolve(process.argv[ 2 ]);
+        const aidenLabAdditions = fs.readFileSync(file, 'utf-8');
+        fs.writeSync(fd, aidenLabAdditions, null, 'utf-8');
+        skipAidenLabAdditions = true;
+    }
+
+    else if(skipAidenLabAdditions) {
+        if(line.includes("<!--AIDEN_LAB")) {
+            skipAidenLabAdditions = false;
+        }
+    }
 
     else {
         fs.writeSync(fd, line + '\n', null, 'utf-8')
